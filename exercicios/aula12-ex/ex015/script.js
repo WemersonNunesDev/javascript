38866236f3ce46639069abc6bcb4cea6078ba4bb
WemersonNunesDev <wemersononunes@gmail.com>
2024-02-28 21:44:04 -0300

    Correção do EX015
    Apaguei o projeto e refiz do zero.

--- exercicios/aula12-ex/ex015/script.js
+++ exercicios/aula12-ex/ex015/script.js
@@ -2,49 +2,48 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano) {
-        alert('[ERRO] Verifique os dados informados novamente')
+    var res = document.querySelector('div#resultado')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByTagName('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-
-        if (fsex[0].checked) {
+        img.setAttribute('img', 'foto')
+        if(fsex[0].checked) {
             gênero = 'Homem'
-
-            if(idade >=0 && idade < 10){
+            if(idade >= 0 && idade < 10) {
                 //criança
                 img.setAttribute('src', './imagens/bebe-menino.jpg')
-            }else if(idade < 21){
+            }else if(idade < 21) {
                 //jovem
                 img.setAttribute('src', './imagens/jovem-homem.jpg')
-            }else if(idade < 50){
+            }else if(idade < 50) {
                 //adulto
                 img.setAttribute('src', './imagens/homem-adulto.jpg')
             }else{
                 //idoso
                 img.setAttribute('src', './imagens/homem-velho.jpg')
             }
-        } else if (fsex[1].checked) {
+        }else if (fsex[1].checked) {
             gênero = 'Mulher'
-
-            if(idade >=0 && idade < 10){
+            if(idade >= 0 && idade < 10) {
                 //criança
-            }else if(idade < 21){
+                img.setAttribute('src', './imagens/bebe-menina.jpg')
+            }else if(idade < 21) {
                 //jovem
-            }else if(idade < 50){
+                img.setAttribute('src', './imagens/jovem-mulher.jpg')
+            }else if(idade < 50) {
                 //adulto
+                img.setAttribute('src', './imagens/mulher-adulta.jpg')
             }else{
                 //idoso
+                img.setAttribute('src', './imagens/mulher-velha.jpg')
             }
         }
-
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-        res.appendChild(img)
+        resultado.style.textAlign = 'center'
+        resultado.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        resultado.appendChild(img)
     }
-
 }
