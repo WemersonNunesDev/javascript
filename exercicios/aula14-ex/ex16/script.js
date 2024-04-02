334e86ec9a731b243534ad8e7f476dfe806ebbfd
WemersonNunesDev <wemersononunes@gmail.com>
2024-04-02 14:00:45 -0300

    Resolução de atividades
    Atualização com a solução das atividades 16 e 17

--- exercicios/aula14-ex/ex16/script.js
+++ exercicios/aula14-ex/ex16/script.js
@@ -1,13 +1,30 @@
-/*
-Definindo as variáveis:
-Inicio - 0
-número final - variavel => definida pelo usuario
-intervalo - variavel => definida pelo usuario
-}*/
+function contar() {
+    var inicio = document.getElementById('txtini')
+    var fim = document.getElementById('txtfim')
+    var intervalo = document.getElementById('txtint')
+    var resultado = document.getElementById('resultado')
 
-//Contador
-for(var c = 0; c <= 15; c+= 3) {
-    console.log(`Contando... ${c}`)
+    if (inicio.value.length == 0 || fim.value.length == 0 || intervalo.value.length == 0) {
+        alert('[ERRO] Faltam dados!')
+    } else {
+        resultado.innerHTML = `Contando: <br>`
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var t = Number(intervalo.value)
+        if (t <= 0) {
+            alert('Intervalo invalido! Considerando INTERVALO 1')
+            t = 1
+        }
+        if (i < f) {
+            for (var c = i; c <= f; c += t) {
+                resultado.innerHTML += `${c} \u{1F449} `
+            }
+            resultado.innerHTML += `\u{1F3C1}`
+        } else {
+            for(let c = i; c >= f; c-= t) {
+                resultado.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
+    }
 }
-
-//-------------------------------------
--- exercicios/aula14-ex/ex17/script.js
+++ exercicios/aula14-ex/ex17/script.js
@@ -1,9 +1,19 @@
-/*
-Inico da tabuada - 0
-Final da tabuada - 10
-Numero multiplicador - variavel definida pelo usuario
-*/
-//Tabuada
-for(var c = 0; c<=10; c+= 1) {
-    console.log(`${c} x 2 = ${c * 2}`)
+function tabuada() {
+    var numero = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+
+    if (numero.value.length == 0) {
+        alert('Por favor, digite um número!')
+    }else{
+        var num = Number(numero.value)
+        let c = 1
+        tab.innerHTML = ``
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
