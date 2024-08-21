59ffed089a247a61c8d937467e8f1465ce4df4be
WemersonNunesDev <wemersononunes@gmail.com>
2024-08-21 17:30:55 -0300

    Updated
    Updated

--- udemy-js/secao8-promises/aula91-JSON/src/js/main.js
+++ udemy-js/secao8-promises/aula91-JSON/src/js/main.js
@@ -1,10 +1,42 @@
-fetch('pessoas.json')
-    .then(resposta => resposta.json())
-    .then(json => carregaEleemntosNaPagina(json));
+axios('pessoas.json')
+    .then(resposta => carregaElementosNaPagina(resposta.data));
 
-function carregaEleemntosNaPagina(json) {
+function carregaElementosNaPagina(json) {
     const table = document.createElement('table');
-    for(let pessoa of json) {
+
+    const trHead = document.createElement('tr');
+
+    let thNome = document.createElement('th');
+    thNome.innerHTML = 'Nome';
+    trHead.appendChild(thNome);
+
+    let thIdade = document.createElement('th');
+    thIdade.innerHTML = 'Idade';
+    trHead.appendChild(thIdade);
+
+    let thSalario = document.createElement('th');
+    thSalario.innerHTML = 'Salario';
+    trHead.appendChild(thSalario);
+    table.appendChild(trHead)
+
+    for (let pessoa of json) {
         const tr = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        td1.innerHTML = pessoa.nome;
+        tr.appendChild(td1)
+
+        let td2 = document.createElement('td');
+        td2.innerHTML = pessoa.idade;
+        tr.appendChild(td2)
+
+        let td3 = document.createElement('td');
+        td3.innerHTML = `R$ ${pessoa.salario.toFixed(2)}`;
+        tr.appendChild(td3)
+
+        table.appendChild(tr)
     }
+
+    const resultado = document.querySelector('.resultado');
+    resultado.appendChild(table)
 }
---
+++ udemy-js/secao9-tooling-ES6/aula92-babel/aula92.js
@@ -0,0 +1,4 @@
+const nome = 'Wemerson';
+const obj = { nome };
+const novoObj = { ...obj }
+console.log(novoObj);
